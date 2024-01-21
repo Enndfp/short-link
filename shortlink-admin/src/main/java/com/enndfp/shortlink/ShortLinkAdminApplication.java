c4cb86bc43a1a4683adc8e6a0c3173a7dd8d9e1a
Enndfp <3193947435@qq.com>
2024-01-21 16:47:37 +0800

    build: 🛠️ 构建项目子模块

---
+++ shortlink-admin/src/main/java/com/enndfp/shortlink/ShortLinkAdminApplication.java
@@ -0,0 +1,15 @@
+package com.enndfp.shortlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Enndfp
+ */
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class,args);
+    }
+}
