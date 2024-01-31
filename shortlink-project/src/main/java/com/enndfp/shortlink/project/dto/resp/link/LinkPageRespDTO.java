27b682eeef0e4f6c6fc4ef26ca43f8479c7e9694
Enndfp <3193947435@qq.com>
2024-01-31 21:20:25 +0800

    feat: ✨ 短链接分页查询接口新增创建时间以及时间序列化

--- shortlink-project/src/main/java/com/enndfp/shortlink/project/dto/resp/link/LinkPageRespDTO.java
+++ shortlink-project/src/main/java/com/enndfp/shortlink/project/dto/resp/link/LinkPageRespDTO.java
@@ -1,5 +1,6 @@
 package com.enndfp.shortlink.project.dto.resp.link;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -55,10 +56,17 @@ public class LinkPageRespDTO {
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date validDate;
 
     /**
      * 描述
      */
     private String description;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createdTime;
 }
